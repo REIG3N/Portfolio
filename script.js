dc7ecba51f389a1407f52fa01280c7c04ec65689
REIG3N <ryan.bardat@edu.devinci.fr>
2022-10-11 09:03:23 +0200

    :art: Ajout des titres des pages présentation et projets

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const STAR1 = document.querySelectorAll('.star1');
 const STAR2 = document.querySelectorAll('.star2');
 const STAR3 = document.querySelectorAll('.star3');
-const BGSCN = document.querySelectorAll('.backgroundScreen');
+const BGSN = document.querySelectorAll('.backgroundScreen');
 
 function StarRandomPosition(){
-    STAR1.style['top'] = Math.random() * BGSCN.style['height'];
-    STAR1.style['left'] = Math.random() * BGSCN.style['width'];
-
-    
+    STAR1.style['top'] = Math.random() * BGSN.style['height'];
+    STAR1.style['left'] = Math.random() * BGSN.style['width'];
 }
