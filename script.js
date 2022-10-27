e9b532460dde963a0dadeb9cfa28dd05f861f013
REIG3N <ryan.bardat@edu.devinci.fr>
2022-10-27 14:25:30 +0200

    :lipstick: :art: :sparkles: Ajout de fichier scss + Bloc Projet

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const STAR1 = document.querySelectorAll('.star1');
-const STAR2 = document.querySelectorAll('.star2');
-const STAR3 = document.querySelectorAll('.star3');
 const BGSN = document.querySelectorAll('.backgroundScreen');
+const SCREENLENGTH = window.screenX;
+const SCREENHEIGHT =  window.screenY ;
 
 function StarRandomPosition(){
-    STAR1.style['top'] = Math.random() * BGSN.style['height'];
-    STAR1.style['left'] = Math.random() * BGSN.style['width'];
+    STAR1.style['top'] = Math.random(SCREENHEIGHT) * SCREENHEIGHT;
+    /*remplacer BGSC par window (element javascript)
+    modifier la valeur de math.random (elle est cappé de 0 à 1) */
+    STAR1.style['left'] = Math.random(SCREENLENGTH) * SCREENLENGTH;
 }
+StarRandomPosition();
