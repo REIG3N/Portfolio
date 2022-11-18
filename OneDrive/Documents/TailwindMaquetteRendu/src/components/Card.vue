ddcf7f789d4c95353211d327e5066ae310af5d79
BARDAT Ryan <105910237+REIG3N@users.noreply.github.com>
2022-11-18 09:39:48 +0100

    first commit

--- OneDrive/Documents/TailwindMaquetteRendu/src/components/Card.vue
+++
@@ -1,9 +0,0 @@
-<!-- Voir https://vuejs.org/guide/components/props.html#prop-passing-details -->
-
-<script setup>
-defineProps(['title'])
-</script>
-
-<template>
-  Mon titre: {{ title }}
-</template>
