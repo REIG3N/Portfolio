5f1b004a7c346c26301c4f6f8a0471a9c10899d5
ryanb <ryan.bardat.123@gmail.com>
2023-07-11 10:50:49 +0200

    :progress: Changing project

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ $(document).ready(function () {
         });
     });
 });
+
+function Menu(e){
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+}
+
+ScrollReveal().reveal('presContainer');
